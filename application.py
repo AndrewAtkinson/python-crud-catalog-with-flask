167af2afef59e5ff6330780b3d0dcde89b2ccb0a
AndrewAtkinson <andrew.atkinson@visualsoft.co.uk>
2016-03-25 12:34:58 +0000

    added setup route and description

--- application.py
+++ application.py
@@ -5,5 +5,13 @@ app = Flask(__name__)
 def hello():
     return "Hello World!!"
 
+@app.route("/setup")
+def setup():
+    return "Hello World!!"
+
 if __name__ == "__main__":
+	''' Run the web server on all external ports (we need this for the vagrant machine)
+		and on port 8000. debugging for now is allowed so changes can be tested quickly
+		and we can see any error messages.
+	'''
     app.run(host='0.0.0.0', port=8000, debug=True)
