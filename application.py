b8bc531f761618bdd8b6e89884f9c95d586b8875
AndrewAtkinson <andrew.atkinson@visualsoft.co.uk>
2016-03-27 22:55:56 +0100

    docblocks

--- application.py
+++ application.py
@@ -20,36 +20,50 @@ CLIENT_ID = json.loads(
     open('client_secrets.json', 'r').read())['web']['client_id']
 APPLICATION_NAME = "Python Catalog"
 
+
 @app.route("/")
 @app.route("/catalog")
 def index():
+	'''The index path of the app to show all categories
+
+	Returns:
+      A rendered index view.
+     '''
 	items = db.get_items()
 	categories = db.get_categories()
 	return render_template('index.html', items=items, categories=categories)
 
+
 @app.route("/catalog.json")
 @app.route("/catalog/catalog.json")
 def catalog_json():
+'''/catalog.json this will display all items as json data'''
 
 	return db.get_items(True)
 
 @app.route("/catalog/categories.json")
 @app.route("/categories.json")
 def categories_json():
-
+'''/categories.json this will display all categories as json data'''
 	return db.get_categories(True)
 
 @app.route("/add")
 def add():
+	'''The add path this shows logged in users the form to add items
+
+	Returns:
+      A rendered form view.
+     '''
 	if(get_user_id() == 0):
 		flash('You must be logged in to add an item', 'error')
 		return redirect(url_for("index"))
 	categories = db.get_categories()
 	return render_template('form.html', categories=categories, item=[])
 
+
 @app.route("/create" , methods=['POST'])
 def create():
-
+	'''The path to handle all new item creations'''
 	if session.get('token', None) != request.form['csrf_token']:
 		flash('Incorrect token', 'error')
 		return redirect(url_for("add"))
@@ -70,6 +84,8 @@ def create():
 
 @app.route("/update" , methods=['POST'])
 def update():
+	'''The add to handle all update requests to items'''
+
 	item = db.get_item(request.form['item_id'])
 
 	if(get_user_id() == 0):
@@ -91,34 +107,84 @@ def update():
 	
 	return redirect(url_for("index"))
 
+
 @app.route("/catalog/<category_name>-cat<int:category_id>")
 def view_category(category_name, category_id):
+	'''Route to show all items in a category using the catgory id to get the category encase the names are duplicated
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+	'''
 	items = db.get_items_by_category_id(category_id)
 	categories = db.get_categories()
 	return render_template('index.html', items=items, categories=categories, category_id=category_id)
 
+
 @app.route("/catalog/<category_name>-cat<int:category_id>/JSON")
 def view_category_json(category_name, category_id):
+	'''Shows all the items in a category as json
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+	'''
 	return db.get_items_by_category_id(category_id, True)
+
+
 
 @app.route("/catalog/<category_name>-cat<int:category_id>/<item_title>-item<int:item_id>")
 def view_item(category_name, category_id, item_title, item_id):
+	'''Shows an item using the item id
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+      item_name:  the name of the item
+      item_id:  the id of the item
+	'''
 	item = db.get_item(item_id)
 	categories = db.get_categories()
 	return render_template('item.html', categories=categories, item=item)
 
 @app.route("/catalog/<category_name>-cat<int:category_id>/<item_title>-item<int:item_id>/JSON")
 def view_item_json(category_name, category_id, item_title, item_id):
+	'''Shows an item as json
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+      item_name:  the name of the item
+      item_id:  the id of the item
+	'''
 	return db.get_item(item_id, True)
+
 
 @app.route("/catalog/<category_name>-cat<int:category_id>/<item_title>-item<int:item_id>/edit")
 def edit_item(category_name, category_id, item_title, item_id):
+	'''Renders the item form to update an item
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+      item_name:  the name of the item
+      item_id:  the id of the item
+	'''
 	item = db.get_item(item_id)
 	categories = db.get_categories()
 	return render_template('form.html', categories=categories, item=item)
 
+
 @app.route("/catalog/<category_name>-cat<int:category_id>/<item_title>-item<int:item_id>/delete")
 def delete_item(category_name, category_id, item_title, item_id):
+	'''Handles a delete request to an item
+
+	Args:
+      category_name:  the name of the category
+      category_id:  the id of the category
+      item_name:  the name of the item
+      item_id:  the id of the item
+	'''
 	item = db.get_item(item_id)
 
 	if(get_user_id() != item.user_id):
@@ -130,18 +196,21 @@ def delete_item(category_name, category_id, item_title, item_id):
 
 @app.route("/setup")
 def setup():
+	'''Generates 10 categories and 10 items for who ever is logged in'''
 	id = get_user_id()
 	db.generate_data(id)
 	return redirect(url_for("index"))
 
 @app.route("/logout")
 def logout():
+	'''destroys the session data to log the user out'''
 	session.clear()
 	flash("You have been successfully logged out", 'success')
 	return redirect(url_for("index"))
 
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
+	'''Handles the gplus login requests'''
     
     #check the token from google with the application token
     if request.args.get('state') != session['state']:
@@ -214,18 +283,21 @@ def gconnect():
 
 
 def generate_state():
+	'''generates a random string to use for google plus'''
 	state = ''.join(random.choice(string.ascii_uppercase + string.digits)
 	                    for x in range(0,32))
 	session['state'] = state
 	return session['state']
 
 def generate_token():
+	'''Generates a random string to use with the forms'''
 	token =  ''.join(random.choice(string.ascii_uppercase + string.digits)
 	                    for x in range(0,32))
 	session['token'] = token
 	return session['token']
 
 def get_user_id():
+	'''gets the user id for the logged in user'''
 	user_id = 0
 	if(session.get('google_plus_id', None) is not None):
 		user_id = session['google_plus_id']
@@ -233,12 +305,15 @@ def get_user_id():
 	return user_id
 
 def get_username():
+	'''gets the username for the logged in user'''
 	username = ''
 	if(session['username'] != ''):
 		username = session['username']
 
 	return username
 
+
+#global vars for our templates
 app.jinja_env.globals['csrf_token'] = generate_token  
 app.jinja_env.globals['STATE'] = generate_state
 app.jinja_env.globals['google_plus_id'] = get_user_id
@@ -251,6 +326,6 @@ if __name__ == "__main__":
 	and we can see any error messages.
 	'''
 	app.secret_key = 'A0Zr98j/3yX T~XH--jmN]LWX/,?RT'
-	app.run(host='0.0.0.0', port=8000, debug=True)
+	app.run(host='0.0.0.0', port=8000)
 
 
